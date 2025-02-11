5b53a2e85897d88f40dc248c6e4197d0a5e8da9d
priyankamaddu <priyankamaddu04@gmail.com>
2025-02-11 14:59:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const sections = document.querySelectorAll(".section");
+    const options = {
+        threshold: 0.3,
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+            }
+        });
+    }, options);
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
